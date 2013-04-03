fad6f9dbe6dfa84e599b963bc162fa017609f0d3
Roman Kozlovskyi <krzroman@gmail.com>
2013-04-03 17:29:48 +0300

    Added navigation portlet with silo

---
+++ quintagroup/silogroup/portlets/navigation.py
@@ -0,0 +1,6 @@
+from plone.app.portlets.portlets.navigation import Renderer
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class NavRenderer(Renderer):
+     recurse = ViewPageTemplateFile('navigation_recurse.pt')
